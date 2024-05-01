3914eb03cf0c359bf55c3f9ef9fc528a7076874b
CrashTagiyev <121178020+CrashTagiyev@users.noreply.github.com>
2024-05-01 22:07:36 +0400

    Add project files.

---
+++ PartialViewComponentHomework/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace PartialViewComponentHomework.ViewComponents
+{
+    public class HeaderViewComponent: Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
